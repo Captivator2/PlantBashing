af85fd243df0fb8e951772228b10f4acebd9c0cf
Vincent is him <toluafuwape2020@gmail.com>
2025-04-29 15:24:37 -0500

    Initial game script

---
+++ PlantBashing/plant_bashing.sh
@@ -0,0 +1,9 @@
+echo "Welcome to PlantBashing! Lets grow something great!"
+echo
+read -p "Whats your name,gardener?" Vincent
+
+echo
+echo "Nice to meet you, $Vincent. Let's get planting!"
+sleep 3
+echo "Goodbye for now, $Vincent!"
+exit 0
