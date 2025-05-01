a4801d4c578c122054806aa42fc9a378c50df5fb
Vincent is him <toluafuwape2020@gmail.com>
2025-05-01 15:16:21 -0500

    Milestone2 yes and no

--- PlantBashing/plant_bashing.sh
+++ PlantBashing/plant_bashing.sh
@@ -7,3 +7,24 @@ echo "Nice to meet you, $Vincent. Let's get planting!"
 sleep 3
 echo "Goodbye for now, $Vincent!"
 exit 0
+echo ""
+read -p "Do you want to plant a new seed? (yes/no):" plant_choice
+
+if [[ "$plant_choice" == "yes" ]]; then
+	echo "You dig a small hole and genlty plant a seed the sze of a raindrop."
+elif [[ "$plant_choice" == "no" ]]; then
+	echo "In this digital garden, time moves faster than your world..."
+	sleep 2
+	read -p "Do you want to wait and see if the seed will grow? (yes/no): " wait_choice
+
+	if [[ "$wait_choice" == "yes" ]]; then
+		echo "Waiting one day..."
+		sleep 1
+	else
+		echo "Goodbye, caretaker. See you next time!"
+		exit 0
+	fi
+else
+	echo "Invalid choice. Please run the game again."
+	exit 1
+fi
